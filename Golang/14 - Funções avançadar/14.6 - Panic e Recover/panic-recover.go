0c7f1fe5fc5a488233d373548d99ae47bb77abf1
IgorAlvaredo <94122674+IgorAlvaredo@users.noreply.github.com>
2023-09-01 21:04:56 -0300

    update

--- Golang/14 - Funções avançadar/14.6 - Panic e Recover/panic-recover.go
+++ Golang/14 - Funções avançadar/14.6 - Panic e Recover/panic-recover.go
@@ -2,6 +2,25 @@ package main
 
 import "fmt"
 
+func recuperrarExecucao() {
+	if r := recover(); r != nil {
+		fmt.Println("Execução recuperada com sucesso!")
+	}
+}
+
+func alunoEstaAprovado(n1, n2 float64) bool {
+	defer recuperrarExecucao()
+	media := (n1 + n2) / 2
+
+	if media > 6 {
+		return true
+	} else if media < 6 {
+		return false
+	}
+	panic("A Média é exatamente 6!")
+}
 func main() {
 	fmt.Println("panic e Recover")
+	fmt.Println(alunoEstaAprovado(8, 6))
+	fmt.Println("Pós execução!")
 }
---
+++ Golang/14 - Funções avançadar/14.9 - init/init.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func init() {
+	fmt.Println("Execução a função init")
+}
+
+func main() {
+	fmt.Println("Função main sendo executada")
+}
